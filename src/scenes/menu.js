9faf24d9d3f431b21edf8870c2421da70284fbb3
RocketKevin <kevinchen91200@gmail.com>
2021-06-25 16:22:46 -0400

    playbutton added

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -11,5 +11,6 @@ export class menu extends Phaser.Scene{
     create(){
         this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.1, "title").setDepth(1);
         this.add.image(0,0, "menubackground").setOrigin(0).setDepth(0);
+        var playbutton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "playbutton").setDepth(1);
     }
 }
