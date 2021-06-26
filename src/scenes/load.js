7f279cc2eedda1387297b8233fd31a9127be2a5e
tylerchen972 <tylerchen972@gmail.com>
2021-06-25 20:32:53 -0400

    load buttons

--- src/scenes/load.js
+++ src/scenes/load.js
@@ -1,29 +1,32 @@
 import { final } from "../final.js";
-export class load extends Phaser.Scene{
-    constructor(){
+export class load extends Phaser.Scene {
+    constructor() {
         super({
             key: final.SCENES.LOAD
         })
     }
-    init(){
+    init() {
 
     }
-    preload(){
+    preload() {
         this.game.canvas.style = "margin: auto; display: block;";
         this.load.image("menubackground", "../assets/menubackground.png");
         this.load.image("title", "../assets/title.png");
         this.load.image("playbutton", "../assets/playbutton.png");
+        this.load.image("controlsbutton", "../assets/controlsbutton.png")
+        this.load.image("optionsbutton", "../assets/optionsbutton.png")
+        this.load.image("quitbutton", "../assets/quitbutton.png")
         var loadingBar = this.add.graphics({
             fillStyle: {
                 color: 0xffffff
             }
         })
-        this.load.on("progress", (percent)=>{
+        this.load.on("progress", (percent) => {
             loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
             console.log(percent);
         })
     }
-    create(){
+    create() {
         this.scene.start(final.SCENES.MENU);
     }
 }
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,16 +1,19 @@
 import { final } from "../final.js";
-export class menu extends Phaser.Scene{
-    constructor(){
+export class menu extends Phaser.Scene {
+    constructor() {
         super({
             key: final.SCENES.MENU
         })
     }
-    init(data){
+    init(data) {
         console.log(data);
     }
-    create(){
+    create() {
         this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.1, "title").setDepth(1);
-        this.add.image(0,0, "menubackground").setOrigin(0).setDepth(0);
-        var playbutton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "playbutton").setDepth(1);
+        this.add.image(0, 0, "menubackground").setOrigin(0).setDepth(0);
+        var playbutton = this.add.image(this.game.renderer.width / 2, 200, "playbutton").setDepth(1);
+        var controls = this.add.image(this.game.renderer.width / 2, 300, "controlsbutton").setDepth(1);
+        var options = this.add.image(this.game.renderer.width / 2, 400, "optionsbutton").setDepth(1);
+        var quit = this.add.image(this.game.renderer.width / 2, 500, "quitbutton").setDepth(1);
     }
 }
