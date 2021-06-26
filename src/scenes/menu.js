9475905bcaca86ea83c3403759cc39d466e2d33b
RocketKevin <kevinchen91200@gmail.com>
2021-06-25 22:43:23 -0400

    buttons graphic added

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -12,8 +12,8 @@ export class menu extends Phaser.Scene {
         this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.1, "title").setDepth(1);
         this.add.image(0, 0, "menubackground").setOrigin(0).setDepth(0);
         var playbutton = this.add.image(this.game.renderer.width / 2, 200, "playbutton").setDepth(1);
-        var controls = this.add.image(this.game.renderer.width / 2, 300, "controlsbutton").setDepth(1);
-        var options = this.add.image(this.game.renderer.width / 2, 400, "optionsbutton").setDepth(1);
+        var controls = this.add.image(this.game.renderer.width / 2, 300, "controlbutton").setDepth(1);
+        var options = this.add.image(this.game.renderer.width / 2, 400, "optionbutton").setDepth(1);
         var quit = this.add.image(this.game.renderer.width / 2, 500, "quitbutton").setDepth(1);
     }
 }
