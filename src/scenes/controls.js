95133520e87e4e7619894f7c6deadb3329aec972
RocketKevin <kevinchen91200@gmail.com>
2021-07-23 19:16:48 -0400

    Simplified (Basic)

--- src/scenes/controls.js
+++ src/scenes/controls.js
@@ -9,7 +9,7 @@ export class controls extends Phaser.Scene {
 
     }
     create() {
-        this.add.image(0, 0, "menubackground").setOrigin(0).setDepth(0);
+        this.add.image(0, 0, "menubackground").setOrigin(0).setDepth(0).setScale(2, 1.25);
         var controlmenu = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "controlmenu").setDepth(1);
         var okay = this.add.image(this.game.renderer.width - 90, this.game.renderer.height - 50, "okaybutton").setDepth(1);
         okay.setInteractive();
--- src/scenes/load.js
+++ src/scenes/load.js
@@ -1,9 +1,10 @@
-import { final } from "../final.js";
+import { final } from "../../final.js";
 export class load extends Phaser.Scene {
     constructor() {
         super({
             key: final.SCENES.LOAD
         })
+        this.loaded = 0;
     }
     init() {
 
@@ -11,13 +12,32 @@ export class load extends Phaser.Scene {
     preload() {
         this.game.canvas.style = "margin: auto; display: block;";
         this.load.json("ItemProperty", "../assets/items/property_items.json");
-        this.load.image("BuyButton", "../assets/shopUI_buyButton.png");
-        this.load.image("HubIcon", "../assets/HubIcon.png");
-        this.load.image("Backpack", "../assets/Backpack.png");
+        this.load.json("MainMenuProperty", "../assets/mainmenu/property_MainMenuComponent.json");
+
+        this.load.image("BuyButton",    "../assets/shopUI_buyButton.png");
+        this.load.image("HubIcon",      "../assets/HubIcon.png");
+        this.load.image("Backpack",     "../assets/Backpack.png");
         this.load.image("BackpackIcon", "../assets/BackpackIcon.png");
-        this.load.image("ShopIcon", "../assets/ShopIcon.png");
-        this.load.image("Shop", "../assets/Shop.png");
-        this.load.image("Hub", "../assets/Hub.png");
+        this.load.image("ShopIcon",     "../assets/ShopIcon.png");
+        this.load.image("Shop",         "../assets/Shop.png");
+        this.load.image("Hub",          "../assets/Hub.png");
+
+        this.load.image("menubackground", "../assets/mainmenu/menubackground.jpg");
+        this.load.image("title",          "../assets/mainmenu/title.png");
+        this.load.image("playbutton",     "../assets/mainmenu/button_play.png");
+        this.load.image("controlbutton",  "../assets/mainmenu/button_control.png");
+        this.load.image("optionbutton",   "../assets/mainmenu/button_option.png");
+        this.load.image("creditsbutton",  "../assets/mainmenu/button_credit.png");
+        this.load.atlas("mainmenu", "../assets/mainmenu/MainMenuComponents.png", "../assets/mainmenu/map_MainMenuComponents.json");
+
+        //this.load.image("yes", "../assets/yes.png");
+        //this.load.image("no", "../assets/no.png");
+        //this.load.image("confirmation", "../assets/confirmation.png");
+        
+        this.load.image("controlmenu", "../assets/controlmenu.png");
+        this.load.image("backbutton", "../assets/back.png");
+        this.load.image("okaybutton", "../assets/okay.png");
+        
         this.load.atlas("dude", "../assets/dude.png", "../assets/dude.json");
         this.load.atlas(
             "items", 
@@ -37,18 +57,6 @@ export class load extends Phaser.Scene {
             "../assets/monsters/slime.png", 
             "../assets/monsters/map_slime.json"
         );
-        this.load.image("menubackground", "../assets/menubackground.jpg");
-        this.load.image("title", "../assets/title.png");
-        this.load.image("playbutton", "../assets/playbutton.png");
-        this.load.image("controlbutton", "../assets/controlbutton.png");
-        this.load.image("optionbutton", "../assets/optionbutton.png");
-        this.load.image("yes", "../assets/yes.png");
-        this.load.image("no", "../assets/no.png");
-        this.load.image("confirmation", "../assets/confirmation.png");
-        this.load.image("creditsbutton", "../assets/credits.png");
-        this.load.image("controlmenu", "../assets/controlmenu.png");
-        this.load.image("backbutton", "../assets/back.png");
-        this.load.image("okaybutton", "../assets/okay.png");
         var loadingBar = this.add.graphics({
             fillStyle: {
                 color: 0xffffff
@@ -57,9 +65,11 @@ export class load extends Phaser.Scene {
         this.load.on("progress", (percent) => {
             loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
             console.log(percent);
+            this.loaded = percent;
         })
     }
     create() {
-        this.scene.start(final.SCENES.MENU);
+        if(this.loaded === 1)
+            this.scene.start(final.SCENES.MENU);
     }
 }
