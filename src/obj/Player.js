1c09be40a14990d27d54b5e9c9841dbe1f91ba0b
RocketKevin <kevinchen91200@gmail.com>
2021-08-15 22:14:31 -0400

    Terrain fully functional for now

--- src/obj/Player.js
+++ src/obj/Player.js
@@ -19,17 +19,16 @@ export class player extends Phaser.Physics.Arcade.Sprite {
     self_config(scene) {
         this.setHitBox(scene);
         this.setStatus();
-        if(scene.test != null)
-            this.collidablesTerrain(scene, scene.test.getMapColliables());
+        if(scene.terrain != null)
+            this.collidablesTerrain(scene, scene.terrain.getMapColliables());
         else {
-            console.log(scene.colliables)
             this.collidablesTerrain(scene, [scene.colliables]);
         }
         this.setScale(0.5);
         this.setOrigin(0, 0);
         this.setFrame("dude1.png");
         this.setDepth(1);
-        this.setCollideWorldBounds(true);
+        // this.setCollideWorldBounds(true);
         this.setImmovable(true);
         this.setSize(30, 40);
         this.setOffset(10, 60);
@@ -41,7 +40,7 @@ export class player extends Phaser.Physics.Arcade.Sprite {
         this.status = new Status();
     }
     collidablesTerrain(scene, collidables) {
-        console.log(collidables);
+        //console.log(collidables);
         for (var i = 0; i < collidables.length; i++) {
             scene.physics.add.collider(this, collidables[i]);
             collidables[i].setCollisionByProperty({ collides: true });
